c9af1ad22d863a376f5e3f9564896f8c2d6e38f3
benny <vvsogi@gmail.com>
2024-09-24 16:26:57 +0900

    Fix error handling in CreateBoard decorator

--- src/services/board/decorator/createBoard.decorator.ts
+++ src/services/board/decorator/createBoard.decorator.ts
@@ -1,9 +1,12 @@
+import typia from 'typia';
 import {
   BadRequestException,
   createParamDecorator,
   ExecutionContext,
+  Logger,
+  NotAcceptableException,
 } from '@nestjs/common';
-import typia from 'typia';
+import { error } from 'console';
 
 export interface CreateBoardDto {
   title: string;
@@ -27,20 +30,36 @@ export const CreateBoard = createParamDecorator(
       const MISSING_DATA = expected && !value;
       const INVALID_DATA_TYPE = typeof expected !== typeof value;
 
-      if (NON_DTO_TYPE)
+      if (NON_DTO_TYPE) {
+        Logger.error(
+          `Received unexpected data '${path.replace(pattern, '')}' [WRONG DATA SENT ERROR] [Create Board]`,
+        );
         throw new BadRequestException(
           `Received unexpected data '${path.replace(pattern, '')}' [WRONG DATA SENT ERROR]`,
         );
+      }
 
-      if (MISSING_DATA)
+      if (MISSING_DATA) {
+        Logger.error(
+          `Received unexpected data '${path.replace(pattern, '')}' [MISSING DATA ERROR] [Create Board]`,
+        );
         throw new BadRequestException(
           `Received unexpected data '${path.replace(pattern, '')}' [MISSING DATA ERROR]`,
         );
+      }
 
-      if (INVALID_DATA_TYPE)
+      if (INVALID_DATA_TYPE) {
+        Logger.error(
+          `Received unexpected data '${path.replace(pattern, '')}' [INVALID TYPE ERROR] [Create Board]`,
+        );
         throw new BadRequestException(
           `Received unexpected data '${path.replace(pattern, '')}' [INVALID TYPE ERROR]`,
         );
+      }
     });
+
+    Logger.error(res.errors);
+    Logger.error(`Not acceptable error [NOT ACCEPTABLE ERROR] [Create Board]`);
+    throw new NotAcceptableException();
   },
 );
