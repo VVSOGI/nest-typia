0b5dd9da2eb3a16b14e511749928ade2d2453d0f
benny <vvsogi@gmail.com>
2024-09-25 17:52:45 +0900

    Add CreateBoardValidator class and import it in index.ts

--- src/services/board/validator/createBoard.validator.ts
+++ src/services/board/validator/createBoard.validator.ts
@@ -1,6 +1,11 @@
 import typia from 'typia';
 import { CreateBoardDto, CustomRequest } from '../decorator';
 
+/**
+ * 이 부분은 원래 Validator로 Generic type을 이용해서 다방면으로 사용할 수 있게 하려고 했으나,
+ * 현재 typia는 generic으로 타입을 전달하면 컴파일 단계에서 막혀버린다.
+ * 아직 해결하지 못한 이슈로 남아있는 것 같아 분리해서 사용하기로 결정.
+ */
 export class CreateBoardValidator {
   private body: CreateBoardDto;
 
